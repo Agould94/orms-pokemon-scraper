36652b243bd41b7ca3b5b058c903c7760f19531f
Alexander Gould <alexander.gould94@gmail.com>
2021-04-06 14:30:18 -0700

    commit tree b3bf7b3d2429831945bc33582f0220e8bcbb9bba

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,35 @@
+require 'pry'
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id: nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        new_pokemon = Pokemon.new(name: name, type: type, db: db)
+            sql = <<-SQL
+            INSERT INTO pokemon(name, type)
+            VALUES (?,?)
+            SQL
+        #binding.pry
+        db.execute(sql, new_pokemon.name, new_pokemon.type)
+        new_pokemon.id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT * FROM pokemon WHERE id = ?
+        SQL
+
+        pkmn = db.execute(sql, id)[0]
+        new_pokemon = Pokemon.new(id: pkmn[0], name: pkmn[1], type: pkmn[2], db: db)
+        new_pokemon
+
+    end
+
+   
 end
